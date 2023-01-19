605c1c75a1605c6f03618bd8e68fa7b15df3f170
daeun han <daeunhan0723@gmail.com>
2023-01-19 23:54:46 +0900

    fix: modify gamecounter view

--- server/apps/games/views.py
+++ server/apps/games/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from server.apps.games.models import User,Game
+from server.apps.games.models import User, Game
 from django.http.request import HttpRequest
 import random
 
@@ -23,17 +23,21 @@ def game_list(request:HttpRequest, *args, **kwargs):
 def game_info(request:HttpRequest, *args, **kwargs):
     pass
 
-def game_counter(request:HttpRequest, *args, **kwargs):
+def game_counter(request:HttpRequest, pk, *args, **kwargs):
+    user = User.objects.get(id=pk)
+    game = Game.objects.get(id=pk)
+    
     a = list(range(1,11))
     num = random.sample(a, 5)
-    context={ "num":num, }
-        
+    context={ 
+        "user":user,
+        "num":num
+    }
     if request.method == "POST":
         Game.objects.create(
             defend_card=request.POST["defend_card"],
         )
-        user_recent = User.objects.last()
-        return redirect(f"/gameinfo/{user_recent.id}")
+        return redirect(f"/gameinfo/{user.pk}")
     return render(request, "games/gamecounter.html", context=context) 
 
 def game_ranking(request:HttpRequest, *args, **kwargs):
