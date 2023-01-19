f088bf53ceb463d02f00b30dfab5d50f0ce39b7f
Daeun Han <daeunhan0723@gamil.com>
2023-01-19 22:22:16 +0900

    feat: gamecounter

--- server/apps/games/views.py
+++ server/apps/games/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from server.apps.games.models import User,Game
 from django.http.request import HttpRequest
+import random
 
 userkey = 3 
 
@@ -23,7 +24,17 @@ def game_info(request:HttpRequest, *args, **kwargs):
     pass
 
 def game_counter(request:HttpRequest, *args, **kwargs):
-    pass
+    a = list(range(1,11))
+    num = random.sample(a, 5)
+    context={ "num":num, }
+        
+    if request.method == "POST":
+        Game.objects.create(
+            defend_card=request.POST["defend_card"],
+        )
+        user_recent = User.objects.last()
+        return redirect(f"/gameinfo/{user_recent.id}")
+    return render(request, "games/gamecounter.html", context=context) 
 
 def game_ranking(request:HttpRequest, *args, **kwargs):
     pass
